099626c3e30e99abad697fcac45677a20efd3634
Karl Frick <karl.frick5@gmail.com>
2021-04-21 14:08:46 -0400

    Initial w/PEDAC

--- Program.cs
+++ Program.cs
@@ -2,11 +2,87 @@
 
 namespace JurasicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            //       create a class dino
+            // dino - 
+            // Name string {get; set;}
+            // DietType string get; set;
+            // WhenAcquired string {get; set;}
+            // Weight int {get; set;}
+            // EnclosureNumber int {get; set;}
+
+            // method to prompt for string response
+            // - read line
+            // - return response
+
+            // method to prompt for int response
+            // - read line
+            // - return response
+
+            // if dino found in park return true
+            // else return NOT FOUND
+
+            // display greeting
+
+            // prompt "what is the dinosuar's name?"
+            // prompt "what is its diet type?"
+            // prompt "when was is acquired?"
+            // prompt "what is its weight?"
+            // prompt "what pen is it located in?"
+
+            // create new dinos list to hold the values of a dino
+            // - add the collections of values to the list
+
+            // create menu
+            // "Choose from the menu. [V]eiw the two types of dinos. [A]dd new dino.
+            // [R]emove a dino. [T]ransfer a dino. [S]ummary to display how many dinos.
+            // [Q]uit.
+
+            // if input = V
+            // Console.WriteLine("{name} is a {diet type}. I was recieved on {date.time}. It weights {weight}.
+            // It is located at {enclosure number}")
+
+            // else if input = A
+            //   var dino = new dino();
+
+            //   prompt name
+            //   prompt diet
+            //   prompt when acquired
+            //   prompt weight
+            //   prompt enclosure number
+
+            //   add properties to dino lsit
+
+            // else if input = R
+            //   var dinoRemove = prompt string for "What dino are you looking for?"
+
+            //   var dinoToRemove = dino by name in dino list with the name equal to the name to be deleted
+            //   if false/null
+            //   return no dino by that name
+
+            //   else dino.remove dino to be deleted
+            //   write dino was removed
+
+            // else if input = T
+            //   var dinoTransfer = prompt string for "what dino do you want to transfer?"
+
+            //   var dinoToTransfer = dino by name in dino list with the name equal to the name to be transfered
+
+            //   if null
+            //   return no dino by that name
+
+            //   else newPenNum = prompt for string "what is {dinoToTransfer}'s new pen number?"
+
+            //   dinoToTransfer.EnclosureNum = newPenNum
+
+            // else if input = S
+            //   need them the number for carno and herbos instend..LINQ 
+
+            // else if inoput = Q
+            //   bool = false // exit
+        }
     }
-  }
 }
